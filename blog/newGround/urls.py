dc0da7240138a233a2feb0819e840468940fb0e9
marcellpaganini <59565705+marcellpaganini@users.noreply.github.com>
2021-01-19 19:03:26 -0400

    update/delete posts

--- blog/newGround/urls.py
+++ blog/newGround/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', HomeView.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
