62fae2fa5932cc2e6cdbc2a6dc13aa9327100a11
marcellpaganini <59565705+marcellpaganini@users.noreply.github.com>
2021-01-23 11:02:32 -0400

    Video Update

--- blog/newGround/models.py
+++ blog/newGround/models.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.TextField()
+    # body = models.TextField()
+    body = RichTextField(blank=True, null=True)
     post_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
